6cc0b79f663e2daee160959c997ce6e2bebbf978
ldg118 <bcvfkd@gmail.com>
2026-03-20 23:45:37 +0800

    Update worker.js

--- worker.js
+++ worker.js
@@ -15,6 +15,7 @@ const GITHUB_URL = 'https://github.com/ldg118/CF-Worker-BestIP';
 
 const CONFIG = {
   sources: [
+//可以自定义IP地址
     'https://ip.164746.xyz',
     'https://cf.090227.xyz',
     'https://monitor.gacjie.cn/page/cloudflare/ipv4.html',
